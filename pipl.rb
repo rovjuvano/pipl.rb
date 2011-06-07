3bac65502d5c3a469c77943b3d99f1159a9001fd
Robert Juliano <rovjuvano@users.sourceforge.net>
2011-06-07 02:27:07 -0400

    move send/read methods into Channel

--- pipl.rb
+++ pipl.rb
@@ -1,54 +1,49 @@
 class Channel
-  def initialize
+  def initialize(pipl)
+    @pipl = pipl
     @queue = []
     @type = :none
   end
 
-  def waiting_for_send?
-    @type == :reader
+  def send(sender)
+puts "[#{self}] send: #{sender}"
+    if waiting_for_send?
+      reader = dequeue :reader
+      @pipl.enqueue_step(sender, reader)
+    else
+      enqueue(sender, :sender)
+    end
   end
 
-  def waiting_for_read?
-    @type == :sender
-  end
-
-  def enqueue_sender(sender)
-    enqueue(sender, :sender)
-  end
-
-  def enqueue_reader(reader)
-    enqueue(reader, :reader)
-  end
-
-  def dequeue_sender
-    dequeue :sender
-  end
-
-  def dequeue_reader
-    dequeue :reader
+  def read(reader)
+puts "[#{self}] read: #{reader}"
+    if waiting_for_read?
+      sender = dequeue :sender
+      @pipl.enqueue_step(sender, reader)
+    else
+      enqueue(reader, :reader)
+    end
   end
 
   private
+    def waiting_for_send?
+      @type == :reader
+    end
+
+    def waiting_for_read?
+      @type == :sender
+    end
+
     def enqueue(item, type)
-      if @type == type || @type == :none
-        @queue.push(item)
-        @type = type
-      else
-        s = type == :sender ? 'readers' : 'senders'
-        raise ArgumentError, "Trying to enqueue #{type} with #{s} in the queue"
-      end
+      @queue.push(item)
+      @type = type
     end
 
     def dequeue(type)
-      if @type == type
-        if @queue.count <= 1
-          @type = :none
-        end
-        @queue.shift
-      else
-        s = @type == :none ? 'nothing' : type == :sender ? 'senders' : 'readers'
-        raise ArgumentError, "Trying to dequeue #{type} with #{s} in the queue"
+      if @queue.count <= 1
+        @type = :none
       end
+      @queue.shift
     end
 end
 
@@ -99,53 +94,47 @@ class PIPL
 
   # programming
   public
-  
-  def send(channel, sender)
-puts "sending: #{sender}"
-    if channel.waiting_for_send?
-      reader = channel.dequeue_reader
-      enqueue_step(sender, reader)
-    else
-      channel.enqueue_sender(sender)
-    end
-  end
 
-  def read(channel, reader)
-puts "reading: #{reader}"
-    if channel.waiting_for_read?
-      sender = channel.dequeue_sender
-      enqueue_step(sender, reader)
-    else
-      channel.enqueue_reader(reader)
-    end
+  def enqueue_step(sender, reader)
+    @steps.push( PIPL::Step.new(sender, reader) )
   end
 
   private
-    def enqueue_step(sender, reader)
-      @steps.push( PIPL::Step.new(sender, reader) )
-    end
-
     def dequeue_step
       @steps.shift
     end
-
 end
 
 #describe "PIPL" do
 #  it "should do stuff" do
     @pipl = PIPL.new
-    @channel = Channel.new
-    @pipl.send(@channel, "1")
+    @channel = Channel.new(@pipl)
+    @channel.send("1")
     @pipl.run
-    @pipl.send(@channel, "2")
+    @channel.send("2")
     @pipl.run
-    @pipl.read(@channel, "a")
+    @channel.read("a")
     @pipl.run
-    @pipl.read(@channel, "b")
+
+    @channel2 = Channel.new(@pipl)
+    @channel2.send("21")
     @pipl.run
-    @pipl.read(@channel, "c")
+    @channel2.send("22")
     @pipl.run
-    @pipl.send(@channel, "3")
+    @channel2.read("2a")
+    @pipl.run
+    @channel2.read("2b")
+    @pipl.run
+    @channel2.read("2c")
+    @pipl.run
+    @channel2.send("23")
+    @pipl.run
+
+    @channel.read("b")
+    @pipl.run
+    @channel.read("c")
+    @pipl.run
+    @channel.send("3")
     @pipl.run
 #  end
 #end
